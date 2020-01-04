ea7ef0c0c5e0182e64e394d5bf67fa1aa872a099
Jeff <jeffrey.miiller@gmail.com>
2020-01-04 14:19:35 -0700

    Add the ability to handled DNS' data compression pointers and add corresponding test.

--- src/DomainNameHandling.py
+++ src/DomainNameHandling.py
@@ -5,6 +5,7 @@ DNS data compression algorithm.
 
 from typing import List
 from io import StringIO, BytesIO
+import struct
 
 
 class DomainNameEncoder:
@@ -50,14 +51,12 @@ class DomainNameDecoder:
     """
 
     @staticmethod
-    def decode_domain_name(data: BytesIO) -> str:
+    def decode_domain_name(reader: BytesIO, copy_of_data: BytesIO) -> str:
         """
         Decode the domain name that was provided when the object was initially created.
 
         :return: the decoded domain name as a str
         """
-        reader: BytesIO = data
-        copy_of_data: BytesIO = data
         string_builder: StringIO = StringIO()
 
         DomainNameDecoder._decode_domain_name_helper(reader, copy_of_data, string_builder)
@@ -74,8 +73,11 @@ class DomainNameDecoder:
         label_length: bytes = reader.read(1)
 
         if label_length != b'\x00':
-            if DomainNameDecoder.is_pointer(label_length):
-                pass #todo add handling for pointers in domain name encodings
+            if DomainNameDecoder._is_pointer(label_length):
+                pointer: int = int.from_bytes(DomainNameDecoder._get_first_six_bits_of_pointer(label_length), 'big')
+                pointer = pointer << 8
+                pointer = pointer + int.from_bytes(reader.read(1), 'big')
+                DomainNameDecoder._decode_pointer(pointer, copy_of_data, string_builder)
             else:
                 DomainNameDecoder._decode_label(reader, copy_of_data, string_builder, int.from_bytes(label_length, 'big'))
 
@@ -97,12 +99,30 @@ class DomainNameDecoder:
         DomainNameDecoder._decode_domain_name_helper(reader, copy_of_data, string_builder)
 
     @staticmethod
-    def is_pointer(label_length: bytes) -> bool:
+    def _decode_pointer(pointer: int, copy_of_data: BytesIO, string_builder: StringIO):
+        new_reader = BytesIO(copy_of_data.getvalue())
+        new_reader.seek(pointer)  # set the new reader to point to the pointer specified location
+        DomainNameDecoder._decode_domain_name_helper(new_reader, copy_of_data, string_builder)
+
+    @staticmethod
+    def _is_pointer(label_length: bytes) -> bool:
         """
         Returns true if the label_length byte is a pointer.
         :param label_length: The label_length byte from a dns encoded domain name
         :return: true if pointer, false otherwise
         """
-        a, b = label_length, b'\xc0'
-        return bytes(a[0] & b[0]) == b'\xc0'
+        bitmask: int = int.from_bytes(b'\xc0', 'big')
+        label_length_as_int: int = int.from_bytes(label_length, 'big')
+        return (label_length_as_int & bitmask) == bitmask
+
+    @staticmethod
+    def _get_first_six_bits_of_pointer(label_length: bytes) -> bytes:
+        bitmask: int = int.from_bytes(b'\x3f', 'big')
+        label_length_as_int: int = int.from_bytes(label_length, 'big')
+        return (label_length_as_int & bitmask).to_bytes(1, 'big')
+
+
+
+
+
 
--- tests/DomainNameHandlingTests.py
+++ tests/DomainNameHandlingTests.py
@@ -16,10 +16,15 @@ class DomainNameHandlingTests(unittest.TestCase):
         Initialize test values used in the tests.
         """
         cls.simple_domain_name: str = "www.cs.ubc.ca"
-        cls.simple_domain_name_encoded: bytearray = b'\x03\x77\x77\x77\x02\x63\x73\x03\x75\x62\x63\x02\x63\x61\x00'
+        cls.simple_domain_name_encoded: bytes = b'\x03\x77\x77\x77\x02\x63\x73\x03\x75\x62\x63\x02\x63\x61\x00'
 
         cls.simple_label: str = "ubc"
-        cls.simple_label_encoded: bytearray = b'\x03\x75\x62\x63'
+        cls.simple_label_encoded: bytes = b'\x03\x75\x62\x63'
+
+        cls.domain_name_with_pointers: str = "www.cbu.cs.ubc.ca"
+        # This encoding has two domain names in it. At the start is www.cs.ubc.ca. After is www.cbu.cs.ubc.ca,
+        # which contains a pointer to the first one.
+        cls.domain_name_with_pointers_encoded = b'\x03www\x02cs\x03ubc\x02ca\x00\x03\x77\x77\x77\x03\x63\x62\x75\xc0\x04'
 
     def test_encode_simple_domain_name(self):
         """
@@ -44,7 +49,7 @@ class DomainNameHandlingTests(unittest.TestCase):
         Test case for decoding a simple domain name.
         """
 
-        result: str = DomainNameDecoder.decode_domain_name(BytesIO(self.simple_domain_name_encoded))
+        result: str = DomainNameDecoder.decode_domain_name(BytesIO(self.simple_domain_name_encoded), BytesIO(self.simple_domain_name_encoded))
 
         self.assertEqual(result, self.simple_domain_name)
 
@@ -55,12 +60,24 @@ class DomainNameHandlingTests(unittest.TestCase):
 
         # Encode then decode
         encoding = DomainNameEncoder.encode_domain_name(self.simple_domain_name)
-        decode_after_encoding= DomainNameDecoder.decode_domain_name(BytesIO(encoding))
+        decode_after_encoding= DomainNameDecoder.decode_domain_name(BytesIO(encoding), BytesIO(encoding))
 
         self.assertEqual(decode_after_encoding, self.simple_domain_name)
 
         # Decode then encode
-        decoding = DomainNameDecoder.decode_domain_name(BytesIO(self.simple_domain_name_encoded))
+        decoding = DomainNameDecoder.decode_domain_name(BytesIO(self.simple_domain_name_encoded), BytesIO(self.simple_domain_name_encoded))
         encode_after_decoding = DomainNameEncoder.encode_domain_name(decoding)
 
         self.assertEqual(encode_after_decoding, self.simple_domain_name_encoded)
+
+    def test_decode_domain_name_with_pointer(self):
+        """
+        Test case for decoding a simple domain name.
+        """
+        reader: BytesIO = BytesIO(self.domain_name_with_pointers_encoded)
+        reader.seek(15)  # navigate to the start of the second domain name, which is the one we want to decode
+        copy_of_data: BytesIO = BytesIO(self.domain_name_with_pointers_encoded)
+
+        result: str = DomainNameDecoder.decode_domain_name(reader, copy_of_data)
+
+        self.assertEqual(result, self.domain_name_with_pointers)
