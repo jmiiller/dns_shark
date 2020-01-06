5de609eccbda72e1c66de51d3d301f5325758abe
Jeff <jeffrey.miiller@gmail.com>
2020-01-06 14:25:53 -0800

    Add to docstrings in DomainNameHandling and rename parameters to increase clarity of methods.

--- src/DomainNameHandling.py
+++ src/DomainNameHandling.py
@@ -38,7 +38,6 @@ class DomainNameEncoder:
         :param label: the domain name label that needs to be compressed and encoded
         :return: None
         """
-
         writer.write(len(label).to_bytes(1, 'big'))
         for char in label:
             writer.write(char.encode('ascii'))
@@ -50,23 +49,28 @@ class DomainNameDecoder:
     """
 
     @staticmethod
-    def decode_domain_name(data: BytesIO, copy_of_data: BytesIO) -> str:
+    def decode_domain_name(data: BytesIO, copy_of_message: BytesIO) -> str:
         """
         Decode the domain name that was provided when the object was initially created.
 
+        :param data: the remaining data to be decoded of the dns message, the domain name should be at the start
+        :param copy_of_message: a copy of the entire dns message, used for handling pointers in domain names.
         :return: the decoded domain name as a str
         """
         string_builder: StringIO = StringIO()
 
-        DomainNameDecoder._decode_domain_name_helper(data, copy_of_data, string_builder)
+        DomainNameDecoder._decode_domain_name_helper(data, copy_of_message, string_builder)
 
         return string_builder.getvalue()[1:]  # we remove the first char, since it is an extra '.'.
 
     @staticmethod
-    def _decode_domain_name_helper(data: BytesIO, copy_of_data: BytesIO, string_builder: StringIO) -> None:
+    def _decode_domain_name_helper(data: BytesIO, copy_of_message: BytesIO, string_builder: StringIO) -> None:
         """
         Helper function to decode domain names. (Used to allow for a mutually recursive solution).
 
+        :param data: the remaining data to be decoded of the dns message, the domain name should be at the start
+        :param copy_of_message: a copy of the entire dns message, used for handling pointers in domain names.
+        :param string_builder: the currently decoded domain name
         :return: None
         """
         label_length: bytes = data.read(1)
@@ -76,45 +80,48 @@ class DomainNameDecoder:
                 pointer: int = int.from_bytes(DomainNameDecoder._get_last_six_bits_of_pointer(label_length), 'big')
                 pointer = pointer << 8
                 pointer = pointer + int.from_bytes(data.read(1), 'big')
-                DomainNameDecoder._decode_pointer(pointer, copy_of_data, string_builder)
+                DomainNameDecoder._decode_pointer(pointer, copy_of_message, string_builder)
             else:
-                DomainNameDecoder._decode_label(data, copy_of_data, string_builder, int.from_bytes(label_length, 'big'))
+                DomainNameDecoder._decode_label(data, copy_of_message, string_builder, int.from_bytes(label_length, 'big'))
 
     @staticmethod
-    def _decode_label(data: BytesIO, copy_of_data: BytesIO, string_builder: StringIO, label_length: int) -> None:
+    def _decode_label(data: BytesIO, copy_of_message: BytesIO, string_builder: StringIO, label_length: int) -> None:
         """
         Decodes a single label of a encoded domain name.
 
+        :param data: the remaining data to be decoded of the dns message, the domain name should be at the start
+        :param copy_of_message: a copy of the entire dns message, used for handling pointers in domain names.
+        :param string_builder: the currently decoded domain name
         :param label_length: the length of the label to be decoded
         :return: None
         """
-
         string_builder.write('.')
 
         for _ in range(label_length):
             char: str = data.read(1).decode('ascii')
             string_builder.write(char)
 
-        DomainNameDecoder._decode_domain_name_helper(data, copy_of_data, string_builder)
+        DomainNameDecoder._decode_domain_name_helper(data, copy_of_message, string_builder)
 
     @staticmethod
-    def _decode_pointer(pointer: int, copy_of_data: BytesIO, string_builder: StringIO):
+    def _decode_pointer(pointer: int, copy_of_message: BytesIO, string_builder: StringIO) -> None:
         """
         Decodes a pointer in a domain name.
 
         :param pointer: the pointer value
-        :param copy_of_data: a copy of the entire dns message, used for handling pointers in domain names.
+        :param copy_of_message: a copy of the entire dns message, used for handling pointers in domain names.
         :param string_builder: the currently decoded domain name string
-        :return:
+        :return: None
         """
-        new_data = BytesIO(copy_of_data.getvalue())
+        new_data = BytesIO(copy_of_message.getvalue())
         new_data.seek(pointer)  # set the new data to point to the pointer specified location
-        DomainNameDecoder._decode_domain_name_helper(new_data, copy_of_data, string_builder)
+        DomainNameDecoder._decode_domain_name_helper(new_data, copy_of_message, string_builder)
 
     @staticmethod
     def _is_pointer(label_length: bytes) -> bool:
         """
         Returns true if the label_length byte is a pointer.
+
         :param label_length: The label_length byte from a dns encoded domain name
         :return: true if pointer, false otherwise
         """
