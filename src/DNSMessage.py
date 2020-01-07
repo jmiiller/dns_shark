1749cf81d82e90462cb55013feb8c2cac06fb825
Jeff <jeffrey.miiller@gmail.com>
2020-01-07 12:24:43 -0800

    Resolve Mypy issues with DNSMessage.py's methods.
    Add missing docstrings.

--- src/DNSMessage.py
+++ src/DNSMessage.py
@@ -1,10 +1,10 @@
-from typing import List
 from src.DNSQuestion import DNSQuestion  # type: ignore
 from src.ResourceRecord import ResourceRecord  # type: ignore
+from typing import List, Optional
 from io import BytesIO
 
 
-class DNSMessage():
+class DNSMessage:
     """
     Handles all logic pertaining to decoding and encoding DNS Messages.
     """
@@ -37,6 +37,7 @@ class DNSMessage():
     def _get_response_value_from_flags(flags: int) -> bool:
         """
         Retrieve the get_response value from flags.
+
         :param flags: an integer representing the flags of a dns message
         :return: value of the get_response field as bool
         """
@@ -46,6 +47,7 @@ class DNSMessage():
     def _get_opcode_value_from_flags(flags: int) -> int:
         """
         Retrieve the opcode value from flags.
+
         :param flags: an integer representing the flags of a dns message
         :return: value of the opcode field as int
         """
@@ -55,6 +57,7 @@ class DNSMessage():
     def _get_authoritative_value_from_flags(flags: int) -> bool:
         """
         Retrieve the authoritative value from flags.
+
         :param flags: an integer representing the flags of a dns message
         :return: value of the authoritative field as bool
         """
@@ -64,6 +67,7 @@ class DNSMessage():
     def _get_is_truncated_from_flags(flags: int) -> bool:
         """
         Retrieve the is_truncated value from flags.
+
         :param flags: an integer representing the flags of a dns message
         :return: value of the is_truncated field as bool
         """
@@ -73,6 +77,7 @@ class DNSMessage():
     def _get_recursion_desired_from_flags(flags: int) -> bool:
         """
         Retrieve the recursion_desired value from flags.
+
         :param flags: an integer representing the flags of a dns message
         :return: value of the recursion_desired field as bool
         """
@@ -82,6 +87,7 @@ class DNSMessage():
     def _get_recursion_available_from_flags(flags: int) -> bool:
         """
         Retrieve the recursion_available value from flags.
+
         :param flags: an integer representing the flags of a dns message
         :return: value of the recursion_available field as bool
         """
@@ -91,6 +97,7 @@ class DNSMessage():
     def _get_rcode_from_flags(flags: int) -> int:
         """
         Retrieve the rcode value from flags.
+
         :param flags: an integer representing the flags of a dns message
         :return: value of the rcode field as int
         """
@@ -100,6 +107,7 @@ class DNSMessage():
     def _read_dns_questions(data: BytesIO, copy_of_data: BytesIO, num_of_questions: int) -> List[DNSQuestion]:
         """
         Read the dns questions from the remaining dns message data.
+
         :param data: the remaining data of the dns message to be processed
         :param copy_of_data: a copy of the entire dns message, used for handling pointers in domain names
         :param num_of_questions: number of questions to decode
@@ -115,6 +123,7 @@ class DNSMessage():
     def _read_resource_records(data: BytesIO, copy_of_data: BytesIO, num_of_records: int) -> List[ResourceRecord]:
         """
         Read the resource records from the remaining dns message data.
+
         :param data: the remaining data of the dns message to be processed
         :param copy_of_data: a copy of the entire dns message, used for handling pointers in domain names
         :param num_of_records: number of records to decode
@@ -128,7 +137,11 @@ class DNSMessage():
 
     def print_dns_question(self, dns_server_ip: str) -> None:
         """
-        Print a dns question/query to stdout.
+        Print a dns query message.
+
+        This includes printing the query id, domain name being asked for by the dns query,
+        the type of record being asked for, and the dns server that is being queried.
+
         :param dns_server_ip: ip address of the dns server the domain name resolution request was sent to.
         :return: None
         """
@@ -137,7 +150,11 @@ class DNSMessage():
 
     def print_message(self) -> None:
         """
-        Print a dns message received from a dns server.
+        Print a dns response message.
+
+        This includes printing the response id, answer records, name server records, additional records,
+        and authoritative value of a dns message.
+
         :return: None
         """
         print('Response ID: ' + str(self.query_id) + ' Authoritative = ' + str(self.authoritative))
@@ -155,13 +172,21 @@ class DNSMessage():
     def print_resource_records_trace(records: List[ResourceRecord]) -> None:
         """
         Print a list of resource records.
+
         :param records: a list of resource records
         :return: None
         """
         for record in records:
             record.print_record_for_trace()
 
-    def get_resource_records_that_match_domain_name_and_type(self, domain_name: str, type: int):
+    def get_answer_records_that_match_domain_name_and_type(self, domain_name: str, type: int) -> List[ResourceRecord]:
+        """
+        Retrieves the answer resource records that contain the supplied domain name and record type.
+
+        :param domain_name: the domain name used to select from the answer resource records
+        :param type: the record type used to select from the answer resource records
+        :return:
+        """
         results = []
 
         for record in self.answer_records:
@@ -170,14 +195,29 @@ class DNSMessage():
 
         return results
 
-    def get_name_server_ip_address(self):
+    def get_name_server_ip_address(self) -> Optional[str]:
+        """
+        Retrieves the first available name server ip address.
+
+        :return: the name server ip address as a string, if present. Otherwise, return None.
+        """
         for name_server_record in self.name_server_records:
-            name_server_ip: str = self.get_name_server_ip_address_from_matching_additional_record(name_server_record)
+            name_server_ip: Optional[str] = self.get_name_server_ip_address_from_matching_additional_record(name_server_record)
 
             if name_server_ip:
                 return name_server_ip
 
-    def get_name_server_ip_address_from_matching_additional_record(self, name_server_record: ResourceRecord) -> str:
+        return None
+
+    def get_name_server_ip_address_from_matching_additional_record(self, name_server_record: ResourceRecord) -> Optional[str]:
+        """
+        Given a name server resource record, search the additional records for a corresponding ip address.
+
+        :param name_server_record: the name server record whose ip address is being searched for
+        :return: the name server ip address, if present. Otherwise, return None.
+        """
         for additional_record in self.additional_records:
             if additional_record.name.lower() == name_server_record.rdata.lower() and additional_record.type == 1:
                 return additional_record.rdata
+
+        return None
