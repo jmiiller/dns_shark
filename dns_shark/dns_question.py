a7efde04bd28dbb5a68328f1c8ce0deeeeca7077
Jeff <jeffrey.miiller@gmail.com>
2020-01-08 21:17:33 -0800

    Add documentation for DNSQuestion's instance variables (i.e. for __init__())

--- dns_shark/dns_question.py
+++ dns_shark/dns_question.py
@@ -5,6 +5,14 @@ from dns_shark.domain_name_handling import DomainNameDecoder
 class DNSQuestion:
     """
     Handles all logic pertaining to decoding and encoding DNS Questions
+
+    Instance Attributes:
+
+        name: the name field of the resource record
+        type: the type field of the resource record
+        response_class: the class field of the resource record
+
+        see https://tools.ietf.org/rfc/rfc1035.txt for more description on the meaning of these fields
     """
 
     def __init__(self, name: str, type: int, response_class: int):
