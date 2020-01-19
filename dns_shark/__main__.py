39532450b4dd79f3c79710a568dfb7b375e97d5c
Jeff <jeffrey.miiller@gmail.com>
2020-01-18 17:07:35 -0800

    Configure main() to catch exceptions thrown by dns_resolver and print the appropriate error message.

--- dns_shark/__main__.py
+++ dns_shark/__main__.py
@@ -5,35 +5,47 @@ from dns_shark.resource_record import ResourceRecord
 from typing import List
 from dns_shark.resolver_core import ResolverCore
 from dns_shark.dns_resolver import Resolver
+from dns_shark.errors.dns_format_error import DNSFormatError
+from dns_shark.errors.dns_name_error import DNSNameError
+from dns_shark.errors.dns_not_implemented_error import DNSNotImplementedError
+from dns_shark.errors.dns_server_failure_error import DNSServerFailureError
+from dns_shark.errors.dns_refused_error import DNSRefusedError
+from dns_shark.errors.dns_no_matching_resource_record_error import DNSNoMatchingResourceRecordError
 
-IPV6_TYPE = 28
-IPV4_TYPE = 1
+
+def main(domain_name: str, dns_server_ip: str, ipv6: bool, verbose: bool):
+
+    try:
+        answers: List[ResourceRecord] = Resolver.ask(domain_name, dns_server_ip, ipv6, verbose)
+    except DNSFormatError as e:
+        print("")
+        print(e)
+    except DNSNameError as e:
+        print("")
+        print(e)
+    except DNSNotImplementedError as e:
+        print("")
+        print(e)
+    except DNSServerFailureError as e:
+        print("")
+        print(e)
+    except DNSRefusedError as e:
+        print("")
+        print(e)
+    except DNSNoMatchingResourceRecordError as e:
+        print("")
+        print(e)
+    else:
+        ResolverCore.print_answers(domain_name, answers)
 
 
-def main():
-
+if __name__ == '__main__':
     parser: ArgumentParser = create_parser()
     args: Namespace = parser.parse_args(sys.argv[1:])
 
     dns_server_ip: str = args.dns_server_ip.pop()
     domain_name: str = args.domain_name.pop()
 
-    if args.ipv6 is None:
-        ipv6: bool = False
-    else:
-        ipv6: bool = True
-
-    if args.verbose is None:
-        verbose: bool = False
-    else:
-        verbose: bool = True
-
-    answers: List[ResourceRecord] = Resolver.ask(domain_name, dns_server_ip, ipv6, verbose)
-
-    ResolverCore.print_answers(domain_name, answers)
+    main(domain_name, dns_server_ip, args.ipv6 is not None, args.verbose is not None)
 
     exit(0)
-
-
-if __name__ == '__main__':
-    main()
