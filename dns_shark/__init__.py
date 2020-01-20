4b1852984b6c69b56515ca5b3df7cfd2bd3f0d32
Jeff <jeffrey.miiller@gmail.com>
2020-01-19 18:44:33 -0800

    Further attempts to get TravisCI working with PyPi.

--- dns_shark/__init__.py
+++ dns_shark/__init__.py
@@ -1,2 +1,2 @@
 # Version of dns_shark package
-__version__ = "1.0.0"
+__version__ = "1.0.2"
--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 import pathlib
 from setuptools import setup
 
-# The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
-# The text of the README file
 README = (HERE / "README.md").read_text()
 
-# This call to setup() does all the work
 setup(
     name="dns_shark",
-    version="1.0.0",
+    version="1.0.2",
     description="A simple DNS resolver that can resolve a domain name to either an IPv4 or IPv6 address.",
     long_description=README,
     long_description_content_type="text/markdown",
